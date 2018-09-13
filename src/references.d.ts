9b74a09e63f09f40ee5da87c17eb443b4023ba2b
Dick Smith <richard.smith@ugroupmedia.com>
2018-09-13 11:47:33 -0400

    cleanup

--- src/references.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
-
-/// <reference path="./typings/java!ZendeskProviderSDK.d.ts" />
-/// <reference path="./typings/java!ZendeskSDK.d.ts" />
-/// <reference path="./typings/objc!ZendeskProviderSDK.d.ts" />
-/// <reference path="./typings/objc!ZendeskSDK.d.ts" />
